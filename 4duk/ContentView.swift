7ec26d149f0551c1f42a8057e79861801131f9d7
Andrey Dunaev <0h4t3a@gmail.com>
2020-03-07 19:59:54 +0300

    Initial Commit

---
+++ 4duk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  4duk
+//
+//  Created by Andrey Dunaev on 07.03.2020.
+//  Copyright © 2020 Andrey Dunaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
